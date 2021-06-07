087ca1d66028f35e04d35324d7bae39e52f29298
ConnorMul <mulhollandconnor@icloud.com>
2021-06-07 15:07:59 -0400

    added about me page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,24 @@
 export default function About() {
     return (
         <div>
-            
+            <h1>poet. songwriter. lovemaker.</h1>
+            <p>I truly came into the world in April of 2015. 
+                Born again after all my pain into a new version of myself. 
+                My heart had broken, I was 19 and alone so i took solace in my journal. 
+                Keeping my pain hidden from the world but letting it dance around on paper. 
+                Watering my roots so I could grow. 
+                5 trips around the sun later, 
+                the mist has begun to rise and I can see clearly for miles and miles. 
+                Twilight shines through my curtains to remind me to continue my journey.
+            </p>
+
+            <p>I’m older than i was then. 
+                24 to be exact. I’m a bottle blonde. 
+                My eyes are still blue, they’ve grown just a bit more tired, 
+                not to say they’re less excited, they’ve just seen so much since then. 
+                I’ve grown but i’m shaped heavily by my past. 
+                I’m living in New York now but my soul belongs to a place unknown.
+                </p>
         </div>
     )
 }
